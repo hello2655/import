1f375eb3294147b4d52791a948a67a4687fa5f03
Owen Chang <owenchang@m-toy.com.tw>
2019-10-02 23:32:35 +0800

    add custome

--- import.py
+++ import.py
@@ -1,6 +1,11 @@
 import random
 
-r = random.randint(1,100)
+s = input('請輸入開始值')
+e = input('請輸入結束值')
+s = int(s)
+e = int(e)
+
+r = random.randint(s,e)
 count = 0
 
 while True:
@@ -12,6 +17,6 @@ while True:
 		print('你猜中了！')
 		break
 	elif num > r:
-		print('這個數字介於1至', num , '之間')
+		print('這個數字介於', s ,'和', num , '之間')
 	elif num < r:
-		print('這個數字介於', num ,'和100之間')
+		print('這個數字介於', num ,'和', e ,'之間')
